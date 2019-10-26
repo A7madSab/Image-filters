446b31ec3f4d656b0215e34d70bba52785e3df6d
A7madSab <ahmadsabry330@gmail.com>
2019-10-26 23:58:00 +0200

    structuring porject

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const jimp = require("jimp")
 
 /* Mean Filter */
-
 // Mean filter with the desired rows & column. Fill all filter values by: 1 / (rows×column).
 const meanFilterColumnSize = 15
 const meanFilterRowSize = 15
@@ -14,26 +13,20 @@ for (let i = 0; i < meanFilterRowSize; i++) {
     }
 }
 
-
-/* Gaussian Filter */
-// Very hard to create the mask using the formula programmatically so I using a build in function.
-const sigma = 16
-
-
-/* Laplacian filter */
-const laplacianFilter = [
-    [0, -1, 0],
-    [-1, 5, -1],
-    [0, -1, 0],
-]
+/* Laplacian filter*/
 Laplacian = (image) => {
+    const laplacianFilter = [
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0],
+    ]
     jimp
         .read(image)
         .then(image => image.convolute(laplacianFilter))
         .then(image => image.write("Result.png"))
 }
 
-/* Mean */
+/* Mean filter*/
 Mean = (image, filter, postProcessing) => {
     jimp.read(image, (err, image) => {
         if (err) throw err;
@@ -43,15 +36,17 @@ Mean = (image, filter, postProcessing) => {
     })
 }
 
+/* Gaussian filter */ // Very hard to create the mask using the formula programmatically so I using a build in function.
 Gaussian = async (i, sigma) => {
+    const sigma = 16
     const image = await jimp.read(i);
     image
         .gaussian(sigma)
         .write('Result.png');
 }
 
-/* Sobel  filter */
-Sobel = async (link) => {
+/* EdgeMagnit filter */
+EdgeMagnit = async (link) => {
     const HorizontalSobelFilter = [
         [-1, -2, -1],
         [0, 0, 0],
@@ -85,7 +80,7 @@ Sobel = async (link) => {
     newImage.write("Sobel.png")
 }
 
-// Mean("Filters.png", meanFilter)                  // calling with mean filter
-// Gaussian("Filters.png", sigma)                   // calling Gaussian with Sigma
-// Laplacian("Filters.png", laplacianFilter)        // calling Laplacian with Sigma
-// Sobel("Filters.png")                             // calling Sobel
+// Mean("Filters.png", meanFilter)      // calling with mean filter
+// Gaussian("Filters.png")              // calling Gaussian with Sigma
+// Laplacian("Filters.png")             // calling Laplacian with Sigma
+// EdgeMagnit("Filters.png")            // calling EdgeMagnit
